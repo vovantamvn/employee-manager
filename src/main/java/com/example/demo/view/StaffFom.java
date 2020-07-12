2781fbe7d65c19142c536b6f0b5a0711422e447c
vovantamvn <vovantam@gmail.com>
2020-07-12 21:12:56 +0700

    Change StaffForm

--- src/main/java/com/example/demo/view/StaffFom.java
+++ src/main/java/com/example/demo/view/StaffFom.java
@@ -6,6 +6,7 @@
 package com.example.demo.view;
 
 import com.example.demo.controller.StaffController;
+import com.example.demo.model.User;
 import java.sql.Date;
 import java.text.SimpleDateFormat;
 import javax.swing.JOptionPane;
@@ -14,14 +15,20 @@ import javax.swing.JOptionPane;
  *
  * @author luongle
  */
-public class staffFom extends javax.swing.JFrame {
+public class StaffFom extends javax.swing.JFrame {
 
     /**
      * Creates new form staffFom
      */
    
-    public staffFom() {
+    private User user;
+    
+    public StaffFom(User user) {
         initComponents();
+        
+        // Sua va cap nhat tren thang user nay
+        // Coi cai demo k dung thi xoa di
+        this.user = user;
     }
 
     /**
@@ -228,8 +235,9 @@ public class staffFom extends javax.swing.JFrame {
         StaffController controller = new StaffController();
         String p1 =pass1.getText().toString();
         String p2 =pass2.getText().toString();
-         controller.thaydoimatkhau(p1, p2);
-        
+        controller.thaydoimatkhau(p1, p2);
+        // Tạo cái thông báo
+        JOptionPane.showMessageDialog(this, "Noi dung");
     }//GEN-LAST:event_thaydoiActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
@@ -246,40 +254,6 @@ public class staffFom extends javax.swing.JFrame {
         
     }//GEN-LAST:event_jButton2ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(staffFom.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(staffFom.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(staffFom.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(staffFom.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new staffFom().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton2;
